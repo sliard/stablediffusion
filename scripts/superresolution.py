82103d68b91e484d5e98782d5a81a2990d0f2113
Samuel Liard <samuel.liard@babbar.tech>
2022-11-25 15:16:59 +0000

    remove watermark

--- scripts/superresolution.py
+++ scripts/superresolution.py
@@ -5,7 +5,6 @@ from PIL import Image
 from omegaconf import OmegaConf
 from einops import repeat, rearrange
 from pytorch_lightning import seed_everything
-from imwatermark import WatermarkEncoder
 
 from ldm.models.diffusion.ddim import DDIMSampler
 from ldm.models.diffusion.ddpm import LatentUpscaleDiffusion, LatentUpscaleFinetuneDiffusion
@@ -61,10 +60,6 @@ def paint(sampler, image, prompt, seed, scale, h, w, steps, num_samples=1, callb
     start_code = torch.from_numpy(start_code).to(
         device=device, dtype=torch.float32)
 
-    print("Creating invisible watermark encoder (see https://github.com/ShieldMnt/invisible-watermark)...")
-    wm = "SDV2"
-    wm_encoder = WatermarkEncoder()
-    wm_encoder.set_watermark('bytes', wm.encode('utf-8'))
     with torch.no_grad(),\
             torch.autocast("cuda"):
         batch = make_batch_sd(
@@ -151,7 +146,6 @@ def parse_args():
     parser.add_argument(
         "--init-img",
         type=str,
-        nargs="?",
         help="path to the input image"
     )
     parser.add_argument(
@@ -220,17 +214,15 @@ def parse_args():
 
 def main(opt):
     print("Upscaling image")
-
     input_image = Image.open(opt.init_img)
     w, h = input_image.size
     print(f"loaded input image of size ({w}, {h})")
-
     sample_path = os.path.join(opt.outdir, "samples")
     os.makedirs(sample_path, exist_ok=True)
     base_count = len(os.listdir(sample_path))
-
     sampler = initialize_model(opt.config, opt.ckpt)
     resize_image = predict(sampler, input_image, opt.prompt, opt.steps, opt.n_samples, opt.scale, opt.seed, opt.ddim_eta, opt.noise_level)
+    print("Upscaling done, save image")
     resize_image.save(os.path.join(sample_path, f"{base_count:05}.png"))
 
 if __name__ == "__main__":
